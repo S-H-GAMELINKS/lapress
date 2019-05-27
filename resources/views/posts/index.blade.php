e610743989731071c01fde45540a7ae01a0ba851
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-05-27 14:36:26 +0900

    using bootstrap card

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,7 +5,13 @@
 @section('content')
 
 @foreach($posts as $post)
-    <p><a href="/posts/{{ $post->id }}">{{ $post->title }}</a></p>
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">{{ $post->title }}</h5>
+            <p class="card-text">{{!! str_limit($post->content, 20) !!}}</p>
+            <a href="/posts/{{ $post->id }}" class="btn btn-primary">記事を読む</a>
+        </div>
+    </div>
 @endforeach
 
 @endsection
