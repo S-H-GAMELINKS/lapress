e5bca8543f9dca5125acaed4f7df4ec745c35f90
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-05-27 14:03:40 +0900

    fix using bootstrap4

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,6 +1,8 @@
 <html>
     <head>
         <title>@yield('title')</title>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link href="css/app.css" rel="stylesheet">
     </head>
     <body>
         @component('components.header')
@@ -8,5 +10,7 @@
         <div class="container">
             @yield('content')
         </div>
+
+        <script src="js/app.js"></script>
     </body>
 </html>
