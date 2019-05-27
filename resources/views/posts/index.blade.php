a45640ed3fdd3ce701107e0a25ca82d4714b6873
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-05-27 13:44:58 +0900

    using layouts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,3 +1,11 @@
+@extends('layouts.layout')
+
+@section('title', 'Laravel Press')
+
+@section('content')
+
 @foreach($posts as $post)
     <p><a href="/posts/{{ $post->id }}">{{ $post->title }}</a></p>
 @endforeach
+
+@endsection
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,5 +1,11 @@
+@extends('layouts.layout')
+
+@section('title', $post->title)
+
+@section('content')
 <h1>{{ $post->title }}</h1>
 
 <div class="container">
     {!! $post->content !!}
 </div>
+@endsection
